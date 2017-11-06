40b2763aa746850a8586aca444cde5c5e8e2c104
MathanM <mathan94maddy@yahoo.com>
2017-11-06 18:14:53 +0530

    deploy

--- src/main/java/Login/DBManager.java
+++ src/main/java/Login/DBManager.java
@@ -5,8 +5,8 @@ import org.springframework.jdbc.datasource.DriverManagerDataSource;
 public class DBManager extends DriverManagerDataSource {
      public DriverManagerDataSource getDataBaseSource(){
        DriverManagerDataSource dataSource= new DriverManagerDataSource();
-       dataSource.setUsername("root");
-       dataSource.setPassword("");
+       dataSource.setUsername("wwwaspii_schools");
+       dataSource.setPassword("maddy302390");
        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
        dataSource.setUrl("jdbc:mysql://145.239.227.138:3306/wwwaspii_neetschools");
        return dataSource;
